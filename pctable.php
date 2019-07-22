1e049c5fa95ddd10c42cb28af6c1341c5b3ea60b
Sergi Rene <s.rene@ucl.ac.uk>
2019-07-22 11:25:12 +0100

    tpc

--- pctable.php
+++ pctable.php
@@ -1,301 +1,301 @@
 <tr>
-<td align="left" width="30%"><a href="">Aakanksha Chowdhery</a></td>
+<td align="left" width="30%">Aakanksha Chowdhery</td>
 <td align="left" width="50%"">Google Brain</td>
 </tr>
 <tr>
-<td align="left" width="30%"><a href="">Akane Sano</a></td>
+<td align="left" width="30%">Akane Sano</td>
 <td align="left" width="50%"">Rice University</td>
 </tr>
 <tr>
-<td align="left" width="30%"><a href="">Anthony Rowe</a></td>
+<td align="left" width="30%">Anthony Rowe</td>
 <td align="left" width="50%"">Carnegie Mellon University</td>
 </tr>
 <tr>
-<td align="left" width="30%"><a href="">Ardalan Amiri Sani</a></td>
+<td align="left" width="30%">Ardalan Amiri Sani</td>
 <td align="left" width="50%"">University of California Irvine</td>
 </tr>
 <tr>
-<td align="left" width="30%"><a href="">Aruna Balasubramanian</a></td>
+<td align="left" width="30%">Aruna Balasubramanian</td>
 <td align="left" width="50%"">Stony Brook University</td>
 </tr>
 <tr>
-<td align="left" width="30%"><a href="">Bhaskar Krishnamachari</a></td>
+<td align="left" width="30%">Bhaskar Krishnamachari</td>
 <td align="left" width="50%"">University of Southern California</td>
 </tr>
 <tr>
-<td align="left" width="30%"><a href="">Bo Chen</a></td>
+<td align="left" width="30%">Bo Chen</td>
 <td align="left" width="50%"">Meraki</td>
 </tr>
 <tr>
-<td align="left" width="30%"><a href="">Brad Campbell</a></td>
+<td align="left" width="30%">Brad Campbell</td>
 <td align="left" width="50%"">University of Virginia</td>
 </tr>
 <tr>
-<td align="left" width="30%"><a href="">Chen Qian</a></td>
+<td align="left" width="30%">Chen Qian</td>
 <td align="left" width="50%"">University of California Santa Cruz</td>
 </tr>
 <tr>
-<td align="left" width="30%"><a href="">Chunyi Peng</a></td>
+<td align="left" width="30%">Chunyi Peng</td>
 <td align="left" width="50%"">Purdue University</td>
 </tr>
 <tr>
-<td align="left" width="30%"><a href="">David Chu</a></td>
+<td align="left" width="30%">David Chu</td>
 <td align="left" width="50%"">Google</td>
 </tr>
 <tr>
-<td align="left" width="30%"><a href="">Dinesh Bharadia</a></td>
+<td align="left" width="30%">Dinesh Bharadia</td>
 <td align="left" width="50%"">University of California San Diego</td>
 </tr>
 <tr>
-<td align="left" width="30%"><a href="">Domenico Giustiniano</a></td>
+<td align="left" width="30%">Domenico Giustiniano</td>
 <td align="left" width="50%"">IMDEA Networks</td>
 </tr>
 <tr>
-<td align="left" width="30%"><a href="">Ehsan Aryafar</a></td>
+<td align="left" width="30%">Ehsan Aryafar</td>
 <td align="left" width="50%"">Portland State University</td>
 </tr>
 <tr>
-<td align="left" width="30%"><a href="">Eric Rozner</a></td>
+<td align="left" width="30%">Eric Rozner</td>
 <td align="left" width="50%"">University of Colorado Boulder</td>
 </tr>
 <tr>
-<td align="left" width="30%"><a href="">Fadel Adib</a></td>
+<td align="left" width="30%">Fadel Adib</td>
 <td align="left" width="50%"">MIT</td>
 </tr>
 <tr>
-<td align="left" width="30%"><a href="">Guoliang Xing</a></td>
+<td align="left" width="30%">Guoliang Xing</td>
 <td align="left" width="50%"">The Chinese University of Hong Kong</td>
 </tr>
 <tr>
-<td align="left" width="30%"><a href="">Hamed Haddadi</a></td>
+<td align="left" width="30%">Hamed Haddadi</td>
 <td align="left" width="50%"">Imperial College London</td>
 </tr>
 <tr>
-<td align="left" width="30%"><a href="">Haitham Hassanieh</a></td>
+<td align="left" width="30%">Haitham Hassanieh</td>
 <td align="left" width="50%"">University of Illinois at Urbana-Champaign</td>
 </tr>
 <tr>
-<td align="left" width="30%"><a href="">Heather Zheng</a></td>
+<td align="left" width="30%">Heather Zheng</td>
 <td align="left" width="50%"">University of Chicago</td>
 </tr>
 <tr>
-<td align="left" width="30%"><a href="">Hun Seok Kim</a></td>
+<td align="left" width="30%">Hun Seok Kim</td>
 <td align="left" width="50%"">University of Michigan</td>
 </tr>
 <tr>
-<td align="left" width="30%"><a href="">Ilias Leontiadis</a></td>
+<td align="left" width="30%">Ilias Leontiadis</td>
 <td align="left" width="50%"">Samsung AI</td>
 </tr>
 <tr>
-<td align="left" width="30%"><a href="">Jiansong Han</a></td>
+<td align="left" width="30%">Jiansong Han</td>
 <td align="left" width="50%"">Zhejiang University</td>
 </tr>
 <tr>
-<td align="left" width="30%"><a href="">Jie Gao</a></td>
+<td align="left" width="30%">Jie Gao</td>
 <td align="left" width="50%"">Stony Brook University</td>
 </tr>
 <tr>
-<td align="left" width="30%"><a href="">Jie Xiong</a></td>
+<td align="left" width="30%">Jie Xiong</td>
 <td align="left" width="50%"">UMass Amherst</td>
 </tr>
 <tr>
-<td align="left" width="30%"><a href="">Joerg Widmer</a></td>
+<td align="left" width="30%">Joerg Widmer</td>
 <td align="left" width="50%"">IMDEA Networks</td>
 </tr>
 <tr>
-<td align="left" width="30%"><a href="">Joshua Smith</a></td>
+<td align="left" width="30%">Joshua Smith</td>
 <td align="left" width="50%"">University of Washington</td>
 </tr>
 <tr>
-<td align="left" width="30%"><a href="">Kannan Srinivasan</a></td>
+<td align="left" width="30%">Kannan Srinivasan</td>
 <td align="left" width="50%"">Ohio State University</td>
 </tr>
 <tr>
-<td align="left" width="30%"><a href="">Karthik Dantu</a></td>
+<td align="left" width="30%">Karthik Dantu</td>
 <td align="left" width="50%"">University of Buffalo</td>
 </tr>
 <tr>
-<td align="left" width="30%"><a href="">Karthik Sundaresan</a></td>
+<td align="left" width="30%">Karthik Sundaresan</td>
 <td align="left" width="50%"">NEC Labs</td>
 </tr>
 <tr>
-<td align="left" width="30%"><a href="">Kassem Fawaz</a></td>
+<td align="left" width="30%">Kassem Fawaz</td>
 <td align="left" width="50%"">University of Wisconsin-Madison</td>
 </tr>
 <tr>
-<td align="left" width="30%"><a href="">Kate Ching-Ju Lin</a></td>
+<td align="left" width="30%">Kate Ching-Ju Lin</td>
 <td align="left" width="50%"">National Chiao Tung University</td>
 </tr>
 <tr>
-<td align="left" width="30%"><a href="">Kaushik Chowdhury</a></td>
+<td align="left" width="30%">Kaushik Chowdhury</td>
 <td align="left" width="50%"">Northeastern University</td>
 </tr>
 <tr>
-<td align="left" width="30%"><a href="">Krishna Chintalapudi</a></td>
+<td align="left" width="30%">Krishna Chintalapudi</td>
 <td align="left" width="50%"">Microsoft</td>
 </tr>
 <tr>
-<td align="left" width="30%"><a href="">Kyle Jamieson</a></td>
+<td align="left" width="30%">Kyle Jamieson</td>
 <td align="left" width="50%"">Princeton</td>
 </tr>
 <tr>
-<td align="left" width="30%"><a href="">Lili Qiu</a></td>
+<td align="left" width="30%">Lili Qiu</td>
 <td align="left" width="50%"">University of Texas at Austin</td>
 </tr>
 <tr>
-<td align="left" width="30%"><a href="">Longfei Shangguan</a></td>
+<td align="left" width="30%">Longfei Shangguan</td>
 <td align="left" width="50%"">Microsoft</td>
 </tr>
 <tr>
-<td align="left" width="30%"><a href="">Lu Su</a></td>
+<td align="left" width="30%">Lu Su</td>
 <td align="left" width="50%"">University of Buffalo</td>
 </tr>
 <tr>
-<td align="left" width="30%"><a href="">Luca Mottola</a></td>
+<td align="left" width="30%">Luca Mottola</td>
 <td align="left" width="50%"">Politecnico di Milano</td>
 </tr>
 <tr>
-<td align="left" width="30%"><a href="">Mahesh K. Marina</a></td>
+<td align="left" width="30%">Mahesh K. Marina</td>
 <td align="left" width="50%"">University of Edinburgh</td>
 </tr>
 <tr>
-<td align="left" width="30%"><a href="">Matthai Philipose</a></td>
+<td align="left" width="30%">Matthai Philipose</td>
 <td align="left" width="50%"">Microsoft</td>
 </tr>
 <tr>
-<td align="left" width="30%"><a href="">Mayank Goel</a></td>
+<td align="left" width="30%">Mayank Goel</td>
 <td align="left" width="50%"">Carnegie Mellon University</td>
 </tr>
 <tr>
-<td align="left" width="30%"><a href="">Mo Li</a></td>
+<td align="left" width="30%">Mo Li</td>
 <td align="left" width="50%"">Nanyang Technological University</td>
 </tr>
 <tr>
-<td align="left" width="30%"><a href="">Morley Mao</a></td>
+<td align="left" width="30%">Morley Mao</td>
 <td align="left" width="50%"">University of Michigan</td>
 </tr>
 <tr>
-<td align="left" width="30%"><a href="">Mythili Vutukuru</a></td>
+<td align="left" width="30%">Mythili Vutukuru</td>
 <td align="left" width="50%"">IIT Bombay</td>
 </tr>
 <tr>
-<td align="left" width="30%"><a href="">Neal Patwari</a></td>
+<td align="left" width="30%">Neal Patwari</td>
 <td align="left" width="50%"">University of Utah</td>
 </tr>
 <tr>
-<td align="left" width="30%"><a href="">Nigel Davies</a></td>
+<td align="left" width="30%">Nigel Davies</td>
 <td align="left" width="50%"">Lancaster University</td>
 </tr>
 <tr>
-<td align="left" width="30%"><a href="">Nilanjan Banerjee</a></td>
+<td align="left" width="30%">Nilanjan Banerjee</td>
 <td align="left" width="50%"">University of Maryland</td>
 </tr>
 <tr>
-<td align="left" width="30%"><a href="">Nirupam Roy</a></td>
+<td align="left" width="30%">Nirupam Roy</td>
 <td align="left" width="50%"">UMD College Park</td>
 </tr>
 <tr>
-<td align="left" width="30%"><a href="">Omid Abari</a></td>
+<td align="left" width="30%">Omid Abari</td>
 <td align="left" width="50%"">University of Waterloo</td>
 </tr>
 <tr>
-<td align="left" width="30%"><a href="">Pengyu Zhang</a></td>
+<td align="left" width="30%">Pengyu Zhang</td>
 <td align="left" width="50%"">Alibaba</td>
 </tr>
 <tr>
-<td align="left" width="30%"><a href="">Peter Steenkiste</a></td>
+<td align="left" width="30%">Peter Steenkiste</td>
 <td align="left" width="50%"">Carnegie Mellon University</td>
 </tr>
 <tr>
-<td align="left" width="30%"><a href="">Rajalakshmi Nandakumar</a></td>
+<td align="left" width="30%">Rajalakshmi Nandakumar</td>
 <td align="left" width="50%"">University of Washington</td>
 </tr>
 <tr>
-<td align="left" width="30%"><a href="">Ranveer Chandra</a></td>
+<td align="left" width="30%">Ranveer Chandra</td>
 <td align="left" width="50%"">Microsoft</td>
 </tr>
 <tr>
-<td align="left" width="30%"><a href="">Riku Jantti</a></td>
+<td align="left" width="30%">Riku Jantti</td>
 <td align="left" width="50%"">Aalto University</td>
 </tr>
 <tr>
-<td align="left" width="30%"><a href="">Robert LiKamWa</a></td>
+<td align="left" width="30%">Robert LiKamWa</td>
 <td align="left" width="50%"">Arizona State University</td>
 </tr>
 <tr>
-<td align="left" width="30%"><a href="">Robin Kravets</a></td>
+<td align="left" width="30%">Robin Kravets</td>
 <td align="left" width="50%"">University of Illinois at Urbana-Champaign</td>
 </tr>
 <tr>
-<td align="left" width="30%"><a href="">Romit Roy Choudhury</a></td>
+<td align="left" width="30%">Romit Roy Choudhury</td>
 <td align="left" width="50%"">University of Illinois at Urbana-Champaign</td>
 </tr>
 <tr>
-<td align="left" width="30%"><a href="">Shyam Gollakota</a></td>
+<td align="left" width="30%">Shyam Gollakota</td>
 <td align="left" width="50%"">University of Washington</td>
 </tr>
 <tr>
-<td align="left" width="30%"><a href="">Suman Banerjee</a></td>
+<td align="left" width="30%">Suman Banerjee</td>
 <td align="left" width="50%"">University of Wisconsin-Madison</td>
 </tr>
 <tr>
-<td align="left" width="30%"><a href="">Sung-Ju Lee</a></td>
+<td align="left" width="30%">Sung-Ju Lee</td>
 <td align="left" width="50%"">Korea Advanced Institute of Science and Technology</td>
 </tr>
 <tr>
-<td align="left" width="30%"><a href="">Swarun Kumar</a></td>
+<td align="left" width="30%">Swarun Kumar</td>
 <td align="left" width="50%"">Carnegie Mellon University</td>
 </tr>
 <tr>
-<td align="left" width="30%"><a href="">Tam Vu</a></td>
+<td align="left" width="30%">Tam Vu</td>
 <td align="left" width="50%"">University of Colorado Boulder</td>
 </tr>
 <tr>
-<td align="left" width="30%"><a href="">Tommaso Melodia</a></td>
+<td align="left" width="30%">Tommaso Melodia</td>
 <td align="left" width="50%"">Northeastern University</td>
 </tr>
 <tr>
-<td align="left" width="30%"><a href="">Victor Bahl</a></td>
+<td align="left" width="30%">Victor Bahl</td>
 <td align="left" width="50%"">Microsoft</td>
 </tr>
 <tr>
-<td align="left" width="30%"><a href="">Xenofon Foukas</a></td>
+<td align="left" width="30%">Xenofon Foukas</td>
 <td align="left" width="50%"">Microsoft</td>
 </tr>
 <tr>
-<td align="left" width="30%"><a href="">Xinbing Wang</a></td>
+<td align="left" width="30%">Xinbing Wang</td>
 <td align="left" width="50%"">Shanghai Jiao Tong University</td>
 </tr>
 <tr>
-<td align="left" width="30%"><a href="">Xinyu Zhang</a></td>
+<td align="left" width="30%">Xinyu Zhang</td>
 <td align="left" width="50%"">University of California San Diego</td>
 </tr>
 <tr>
-<td align="left" width="30%"><a href="">Y. Charlie Hu</a></td>
+<td align="left" width="30%">Y. Charlie Hu</td>
 <td align="left" width="50%"">Purdue University</td>
 </tr>
 <tr>
-<td align="left" width="30%"><a href="">Yasamin Mostofi</a></td>
+<td align="left" width="30%">Yasamin Mostofi</td>
 <td align="left" width="50%"">University of California Santa Barbara</td>
 </tr>
 <tr>
-<td align="left" width="30%"><a href="">Yingyan Lin</a></td>
+<td align="left" width="30%">Yingyan Lin</td>
 <td align="left" width="50%"">Rice University</td>
 </tr>
 <tr>
-<td align="left" width="30%"><a href="">Yingying (Jennifer) Chen</a></td>
+<td align="left" width="30%">Yingying (Jennifer) Chen</td>
 <td align="left" width="50%"">Rutgers University</td>
 </tr>
 <tr>
-<td align="left" width="30%"><a href="">Yuanjie Li</a></td>
+<td align="left" width="30%">Yuanjie Li</td>
 <td align="left" width="50%"">HP Labs</td>
 </tr>
 <tr>
-<td align="left" width="30%"><a href="">Yunhao Liu</a></td>
+<td align="left" width="30%">Yunhao Liu</td>
 <td align="left" width="50%"">Tsinghua University</td>
 </tr>
 <tr>
-<td align="left" width="30%"><a href="">Yunxin Liu</a></td>
+<td align="left" width="30%">Yunxin Liu</td>
 <td align="left" width="50%"">Microsoft</td>
 </tr>
 <tr>
