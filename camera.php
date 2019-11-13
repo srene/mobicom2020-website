596796a32e0630d96a3a8c56a96b9690ae9d69a3
srenevic <s.rene@ucl.ac.uk>
2019-11-13 09:01:41 +0000

    camera ready

---
+++ camera.php
@@ -0,0 +1,161 @@
+<?php require_once('head.php'); ?>
+
+<body>
+
+<center>
+<?php require_once('nav.php'); ?>
+<?php require_once('banner-small.php'); ?>
+
+<div class="container bigc">
+<div class="spacing"></div>
+
+<main>
+<h2 class="text-center">Camera-Ready Instructions</h2>
+
+<h1>Camera-Ready Instructions</h1>
+<p>Camera-ready papers for the Mobicom 2020 proceedings is yet to be announced. In essence, you need to do the following:</p>
+<ul>
+  <li>Work with the shepherd to address all the reviewers’ comments. The shepherd will be ultimately responsible for accepting or rejecting your submission.</li>
+  <li>Fill out copyright form, information on how to do this will be emailed to authors automatically in a separate mail.</li>
+  <li>Adapt your stylesheet to the most recent ACM template, include bibliographic and copyright strip.</li>
+  <li>Authors should follow the guide to the MobiCom 2020 submission preparation instructions available at this link: <a href="">TBC</a></li>
+</ul>
+
+<b>Note that conference proceedings will be published in 2 batches before the conference start date.</b>
+
+<h4>Complete rights management form</h4>
+<ul>
+  <li>Before submitting camera-ready papers, you need to complete the rights management form. You will be notified by email once these forms are available and ready to sign.</li>
+  <li>As rights management emails are sent from an automated system, there is a chance that emails sent will wind up in SPAM folders. Please make sure that you and your authors set email SPAM settings to allow emails from “rightsreview@acm.org”.</li>
+</ul>
+<h4>Format paper</h4>
+<ul> <li>Papers should be no more than 12 pages single-spaced pages for text and figures + 4 pages for references.</li>
+  <li>Camera-ready papers should adhere to the ACM templates that can be found here: <a href="http://www.acm.org/publications/proceedings-template">http://www.acm.org/publications/proceedings-template</a>. Templates are provided for formatting papers in MS Word and LaTeX. In LaTeX, you should use <code>\documentclass[sigconf]{acmart}.</code></li>
+  <li>Note that the originally submission instructions suggested using the old ACM template. The font size in the submission template was 10pt, the new template will use 9pt. Using the new ACM template means that you have more space, to address the comments by the reviewers. Please also double check to make sure that you are using the new template (linked in the previous bullet).</li>
+  <li>Please do NOT number the pages of your camera-ready paper (by default page numbers are disabled in the ACM template).</li>
+  <li>You should provide proper indexing information in the final version according to the ACM Computing Classification System (CCS). More information about the ACM CCS is available on the ACM CCS website.</li>
+  <li>After completing their rights management form, you will be emailed the correct rights management text and bibliographic strip to place within your paper. It is essential that you add the text into your paper.</li>
+  <li>Avoid using any special characters or non-standard fonts. We must be able to display and print your submission exactly as we receive it using only standard tools and printers, so we strongly suggest that you use only standard fonts embedded in the PDF file.</li>
+</ul>
+<h4>Submit camera-ready version</h4>
+<ul>
+  <li>TBA</li>
+</ul>
+<h4>Questions</h4>
+<p>If you have any questions about the camera-ready process, feel free to contact Mobicom 2020 publication co-chairs Onur Ascigil  (<a href="mailto:o.ascigil@ucl.ac.uk">o.ascigil@ucl.ac.uk</a>) or Jagmohan Chauhan (<a href="mailto:jc2161@cam.ac.uk">jc2161@cam.ac.uk</a>).</p>
+
+<!--<div class="acceptedpapers">
+<ul>
+
+<li><b>Detecting if LTE is the Bottleneck with BurstTracker</b> <a href="http://web.mit.edu/arjunvb/pubs/bursttracker-mobicom19-paper.pdf">[Preprint]</a><br/>
+<div class="pauthors">Arjun Balasingam (Stanford University), Manu Bansal (Stanford Univeristy), Rakesh Misra (Uhana Inc.), Kanthi Nagaraj (Stanford University), Rahul Tandra (Uhana Inc.), Sachin Katti (Stanford University), Aaron Schulman (UC San Diego)</div></li>
+<br/>
+
+<li><b>Rebooting Ultrasonic Positioning Systems for Ultrasound-incapable Smart Devices</b> <a href="
+https://arxiv.org/pdf/1812.02349.pdf">[Preprint]</a><br/>
+<div class="pauthors">Qiongzheng Lin, Zhenlin An, Lei Yang (The Hong Kong Polytechnic University)</div></li>
+<br/>
+
+<li><b>Wideband Full-Duplex Wireless via Frequency-Domain Equalization: Design and Experimentation</b> <a href="https://arxiv.org/pdf/1812.01126.pdf">[Preprint]</a><br/>
+<div class="pauthors">Tingjun Chen, Mahmood Baraani Dastjerdi (Columbia University), Jin Zhou (University of Illinois at Urbana-Champaign), Harish Krishnaswamy, Gil Zussman (Columbia University)</div></li>
+<br/>
+
+<li><b>AMP up your mobile web experience: Characterizing the impact of Google’s Accelerated Mobile Project</b><br/>
+<div class="pauthors">Byungjin Jun, Fabián E. Bustamante, Sung Yoon Whang (Northwestern University), Zachary S. Bischof (IIJ Research)</div></li>
+<br/>
+
+<li><b>Living IoT: A Flying Wireless Platform on Live Insects</b> <a href="https://homes.cs.washington.edu/~gshyam/Papers/living-iot.pdf">[Preprint]</a> <a href="https://www.youtube.com/watch?v=0_i0K3jcp8Y">[Video]</a><br/>
+<div class="pauthors">Vikram Iyer, Rajalakshmi Nandakumar, Anran Wang, Sawyer B. Fuller, Shyamnath Gollakota (University of Washington)</div></li>
+<br/>
+
+<li><b>ClientMarshal: Regaining Control from Wireless Clients for Better Experience in Enterprise WLANs</b>
+<a href="https://www.researchgate.net/publication/329933591_ClientMarshal_Regaining_Control_from_Wireless_Clients_for_Better_Experience_in_Enterprise_WLANs">[Preprint]</a><br/>
+<div class="pauthors">Apurv Bhartia, Bo Chen, Derrick Pallas, Waldin Stone (Cisco Meraki)</div></li>
+<br/>
+
+<li><b>Systematic way to LTE Testing</b> <a href="https://www.researchgate.net/publication/329542996_A_Systematic_Way_to_LTE_Testing">[Preprint]</a><br/>
+<div class="pauthors">Muhammad Taqi Raza, Songwu Lu (UCLA)</div></li>
+<br/>
+
+<li><b>mD-Track: Leveraging Multi-Dimensionality for Passive Indoor Wi-Fi
+Tracking</b> <a href="https://arxiv.org/pdf/1812.03103.pdf">[Preprint]</a><br/>
+<div class="pauthors">Yaxiong Xie (Nanyang Technological University), Jie Xiong (UMass Amherst), Mo Li (Nanyang Technological University), Kyle Jamieson (Princeton University)</div></li>
+<br/>
+
+<li><b>Experience: Understanding Long-Term Evolving Patterns of Shared Electric Vehicle Networks</b> <a href="https://arxiv.org/pdf/1812.07499.pdf">[Preprint]</a><br/>
+<div class="pauthors">Guang Wang, Xiuyuan Chen (Rutgers University), Fan Zhang (Shenzhen Institutes of Advanced Technology, Chinese Academy of Sciences), Yang Wang (University of Science and Technology of China), Desheng Zhang (Rutgers University)</div></li>
+<br/>
+
+<li><b>MP-H2: a Client-only Multipath Solution for HTTP/2</b> <a href="
+https://anikravesh.github.io/files/mph2-mobicom19.pdf">[Preprint]</a><br/>
+<div class="pauthors">Ashkan Nikravesh, Yihua Guo, Xiao Zhu (University of Michigan), Feng Qian (University of Minnesota - Twin Cities), Z. Morley Mao (University of Michigan)</div></li>
+<br/>
+
+<li><b>VeMo: Enabling Transparent Vehicular Mobility Modeling at Individual Levels with Full Penetration</b> <a href="https://arxiv.org/pdf/1812.02780.pdf">[Preprint]</a><br/>
+<div class="pauthors">Yu Yang, Xiaoyang Xie, Zhihan Fang (Rutgers University), Fan Zhang (Shenzhen Institute of Advanced Technology), Yang Wang (University of Science and Technology of China), Desheng Zhang (Rutgers University)</div></li>
+<br/>
+
+<li><b>SolarGest: Ubiquitous and Battery-free Gesture Recognition using Solar Cells</b> <a href="https://arxiv.org/pdf/1812.01766.pdf">[Preprint]</a><br/>
+<div class="pauthors">Dong Ma, Guohao Lan, Mahbub Hassan, Wen Hu, Mushfika Baishakhi Upama, Ashraf Uddin (University of New South Wales), Moustafa Youssef (Egypt-Japan University of Science and Technology)</div></li>
+<br/>
+
+<li><b>Fire in Your Hands: Understanding Thermal Behavior of Smartphones</b> <a href="https://nmsl.kaist.ac.kr/pdf/MobiCom2019_Thermal_preprint.pdf">[Preprint]</a><br/>
+<div class="pauthors">Soowon Kang (KAIST), Hyeonwoo Choi (Samsung Electronics), Soo Young Park (KAIST), Chunjong Park (University of Washington), Jemin Lee, Uichin Lee, Sung-Ju Lee (KAIST)</div></li>
+<br/>
+
+<li><b>Jigsaw: Robust Live 4K Video Streaming</b><br/>
+<div class="pauthors">Ghufran Baig, Jian He, Mubashir Adnan Qureshi, Lili Qiu (The University of Texas at Austin), Peng Chen, Yinliang Hu (Huawei)</div></li>
+<br/>
+
+<li><b>Diagnosing Vehicles with Automotive Batteries</b><br/>
+<div class="pauthors">Liang He (University of Colorado Denver), Linghe Kong, Ziyang Liu (Shanghai Jiao Tong University), Yuanchao Shu (Microsoft Research), Cong Liu (University of Texas at Dallas)</div></li>
+<br/>
+
+<li><b>PDVocal: Towards Privacy-preserving Parkinson's Disease Early Detection using Passive Body Sounds in Daily Life</b> <a href="https://sctracy.github.io/chensong.github.io/pdf/mobicom19.pdf">[Preprint]</a><br/>
+<div class="pauthors">Hanbin Zhang, Chen Song, Aosen Wang (University at Buffalo), Dongmei Li (University of Rochester Medical Center), Wenyao Xu (University at Buffalo)</div></li>
+<br/>
+
+<li><b>Taprint: Secure Text Input for Commodity Smart Wearables</b> <a href="https://github.com/winston110110/Taprint/blob/master/Taprint.pdf">[Preprint]</a><br/>
+<div class="pauthors">Wenqiang Chen, Lin Chen, Yandao Huang (Shenzhen University), Xinyu Zhang (University of California San Diego), Lu Wang, Rukhsana Ruby, Kaishun Wu (Shenzhen University)</div></li>
+<br/>
+
+<li><b>An Active-Passive Measurement Study of TCP Performance over LTE on High-speed Rails</b><br/>
+<div class="pauthors">Jing Wang, Yufan Zheng, Chenren Xu, Yunzhe Ni, Wangyang Li, Yihua Cheng, Zhuo Cheng (Peking University), Feng Qian (University of Minnesota - Twin Cities), Yuanjie Li (University of California, Los Angeles), Xiufeng Xie (University of Michigan), Yi Sun (Chinese Academy of Sciences), Zhongfeng Wang (Chinese Academy of Railway)</div></li>
+<br/>
+
+<li><b>Mobile Gaming on Personal Computers with Direct Android Emulation</b> <a href="http://www.greenorbs.org/people/lzh/papers/[MobiCom'19]%20Direct%20Android%20Emulation.pdf">[Preprint]</a><br/>
+<div class="pauthors">Qifan Yang, Zhenhua Li (Tsinghua University), Tianyin Xu (UIUC), Ennan Zhai (Alibaba Group Inc.), Yunhao Liu (Tsinghua University), Yuanchao Huang, Jiaming He, Hai Long (Tencent Security)</div></li>
+<br/>
+
+<li><b>OFDMA-Enabled Wi-Fi Backscatter</b> <a href="https://www.dropbox.com/s/065oh0vxw4xwjvp/Mobicom19_OFDMA_BS_FN.PDF?dl=0">[Preprint]</a><br/>
+<div class="pauthors">Renjie Zhao, Fengyuan Zhu, Siyuan Peng, Yuda Feng, Xiaohua Tian, Hui Yu, Xinbing Wang (Shanghai Jiao Tong University)</div></li>
+<br/>
+
+<li><b>Proximity detection with single-antenna IoT devices</b> <a href="https://www.cs.dartmouth.edu/~dfk/papers/pierson-snap.pdf">[Preprint]</a><br/>
+<div class="pauthors">Timothy J Pierson Travis Peters, Ronald Peterson, David Kotz (Dartmouth College)</div></li>
+<br/>
+
+<li><b>Keep Others From Peeking At Your Mobile Device Screen!</b><br/>
+<div class="pauthors">Chun-Yu Chen, Bo-Yao Lin, Junding Wang, Kang G. Shin (University of Michigan, Ann Arbor)</div></li>
+<br/>
+
+<li><b>Towards Touch-to-Access Device Authentication Using Body Electric Potentials</b> <a href="http://arxiv.org/pdf/1902.07057">[Preprint]</a><br/>
+<div class="pauthors">Zhenyu Yan, Qun Song, Rui Tan (Nanyang Technological University, Singapore), Yang Li (Shenzhen University, P. R. China), Adams Wai Kin Kong (Nanyang Technological University, Singapore)</div></li>
+<br/>
+
+<li><b>SignSpeaker: A Real-time, High-Precision SmartWatch-based Sign Language Translator</b><br/>
+<div class="pauthors">Jiahui Hou (University of Science and Technology of China & Illinois Institute of Technology), Xiang-Yang Li, Peide Zhu, Zefan Wang (University of Science and Technology of China), Yu Wang (University of North Carolina at Charlotte), Jianwei Qian (Illinois Institute of Technology), Panglong Yang (University of Science and Technology of China)</div></li>
+<br/>
+
+<li><b>Edge Assisted Real-time Object Detection for Mobile Augmented Reality</b> <a href="http://www.winlab.rutgers.edu/~luyang/papers/mobicom19_augmented_reality.pdf">[Preprint]</a><br/>
+<div class="pauthors">Luyang Liu, Hongyu Li, Marco Gruteser (WINLAB, Rutgers University)</div></li>
+
+</ul>-->
+</div>
+
+</center>
+</main>
+<?php require_once('footer2.php'); ?>
+
+</body>
+</html>
