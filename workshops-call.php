f631be06070a81e306e123682391ed917f375d9a
srenevic <s.rene@ucl.ac.uk>
2020-01-22 10:02:38 +0100

    test

--- workshops-call.php
+++ workshops-call.php
@@ -3,94 +3,91 @@
 <body>
 
 <center>
+<?php require_once('nav.php'); ?>
 <?php require_once('banner-small.php'); ?>
 
-<?php require_once('nav.php'); ?>
+<div class="container bigc">
+<div class="spacing"></div>
 
 <main>
-<h2 class="text-center">Call for Workshops</h2>
+<h2 class="text-center">Call for Workshops and Tutorials</h2>
 
-<p>ACM MobiCom is the premier international conference dedicated to addressing emerging topics in mobile and wireless networking and computing. MobiCom 2019 will hold multiple full or half day workshops on October 21 (Monday), 2019 and October 25 (Friday), 2019 of the conference week. Workshop papers will be included with the MobiCom proceedings and published in the ACM Digital Library.</p>
+ACM MobiCom is the premier international conference dedicated to addressing emerging topics in mobile and wireless networking and computing. MobiCom 2020 will hold multiple full or half day workshops and tutorials on September 21 (Monday), 2020 and September 25 (Friday), 2020 of the conference week. Workshop papers will be included with the MobiCom proceedings and published in the ACM Digital Library.
+<br/><br/>
 
-<p>We solicit workshop proposals on new and emerging topics that broadly relate to the general areas of mobile communications, wireless networking and mobile computing.</p>
+We solicit workshop and tutorial proposals on new and emerging topics that broadly relate to the general areas of mobile communications, wireless networking and mobile computing.
+<br/><br/>
 
-<p>The ideal workshop proposal will focus on a timely and emerging topic and be able to attract a number of interesting and high-quality contributions that spark discussions in an informal setting. The goal of workshop papers is primarily to stimulate interactive conversations and inspire new research ideas. Proposals on emerging or disruptive topics that are likely to generate significant interest in the community will be selected. </p>
 
-<p>Furthermore, given that this is the first MobiCom and SIGMOBILE event to be held in Mexico, we especially solicit workshops on topics with high local & regional relevance (e.g., to Mexico/Central/South America & emerging economies).</p>
 
-<p>MobiCom 2019 will likely feature a mix of some returning and some new workshops. Workshop proposals may be either research-track or industrial-track:</p>
+<h5><b>Workshop Guidelines</b></h5>
+The ideal workshop proposal will focus on a timely and emerging topic and be able to attract a number of interesting and high-quality contributions that spark discussions in an informal setting. The goal of the workshop papers is primarily to stimulate interactive conversations and inspire new research ideas. Proposals on emerging or disruptive topics that are likely to generate significant interest in the community will be selected.
+<br/><br/>
+MobiCom 2020 will likely feature a mix of some returning and some new workshops.  Workshop proposals may be either research-oriented or experimental:
+<br/><br/>
 
-<b>Research-Track Workshops</b><br/>
-<p>We encourage more conventional research-oriented workshops that address compelling and growing fields of study. Topics should be grounded and clearly relate to the ACM MobiCom community, and its core areas of interests (e.g., wireless networking and mobile computing). However, we anticipate workshop proposals will also introduce and connect to a set of topics of broader importance, including but not limited to: autonomous cars and systems, healthcare and medical domains, safety/security/privacy, distributed ledgers and cryptocurrencies, machine learning/artificial intelligence, diversity and ethics, technology issues for developing regions, and novel radio/hardware/processor architecture technologies.</p>
+<b>Research-oriented Workshops</b> <br/>
+We encourage more conventional research-oriented workshops that address compelling and growing fields of study. Topics should be grounded and clearly relate to the ACM MobiCom community, and its core areas of interests (e.g., wireless networking and mobile computing). However, we anticipate workshop proposals will also introduce and connect to a set of topics of broader importance, including but not limited to: autonomous cars and systems, healthcare and medical domains, safety/security/privacy, distributed ledgers and cryptocurrencies, machine learning/artificial intelligence, diversity and ethics, technology issues for developing regions, and novel radio/hardware/processor architecture technologies.
+<br/><br/>
+<b>Experimental Workshops</b> <br/>
+We also invite workshop proposals that would target papers presenting extensive experiences with implementation, deployment, and operation of mobile computing and wireless networks.
+<br/><br/>
 
-<b>Industrial-Track Workshops</b><br/>
-<p>In addition to the research-track workshops detailed above, we encourage proposals that are more oriented towards industry. Examples include (but are not limited to) workshops themed around (i) a specific industry vertical or pertaining to specific emerging standards (e.g., automotive, smart homes, and / or healthcare); and, (ii) hands-on experience workshops involving mobile devices, sensor platforms, and / or  wearables.</p>
-
-<h5>Proposal Submission Instructions</h5>
-
+<b>Workshop Proposal Submission Instructions</b> <br/>
 The proposal should be no longer than three pages and should include at least the following:
-
-<ul>
+<ul style="text-align:left">
 <li>The name of the workshop.</li>
-
-<li>Proposal track (research or industrial) in the title</li>
-
-<li>The name, address, and short bio (up to 200 words) of each organizer (up to three).</li>
-
+<li>Proposal track (research-oriented or experimental) in the title.</li>
+<li>The name, address, and a short bio (up to 200 words) of each organizer (up to three).</li>
 <li>A motivation and rationale that includes a brief description of the technical issues to be addressed, and why those issues are timely.</li>
-
 <li>The list of names and affiliations of potential program committee members.</li>
-
-<li>The planned format of the workshop, including a strategy to facilitate lively discussions and attendee involvement.</li>
-
+<li>The planned format of the workshop, including the length of the workshop (half day or full day), and a strategy to facilitate lively discussions and attendee involvement.</li>
 <li>If previous editions have been held, a description of past versions of the workshop, including the number of submitted and accepted papers, and number of attendees.</li>
-
 <li>A description of the publicity plan.</li>
-
 <li>A call for papers with workshop deadlines (aligned with the relevant Important Dates below).</li>
-</ul>
+<br/><br/>
 
-<h5>Submissions</h5>
-<p>Please send your proposal in PDF (only) via e-mail to both the workshop co-chairs Jennifer Chen and Rajesh Balan (contact details are provided below). Ensure that proposals contain "MobiCom 2019 Workshop Proposal" in the subject line. Submissions must be received by the deadline below.</p>
+<h5><b>Tutorial Guidelines</b></h5>
+Tutorial topics should be of significant interest to the MobiCom community.  Tutorials may consist of lectures, interactive sessions, hands-on training, or any combination of the above.
+<br/><br/>
 
-<h5>Important Dates</h5>
-<div class="table-responsive">
-<table class="table table-striped table-sm datetable">
-<tr>
-<th>Deadlines</th>
-<th></th>
-</tr>
-<tr>
-<td>Proposal Submission Deadline</td>
-<td style="white-space: wrap;">March 8th, 2019, 11:59 PM PST (UTC-8:00)</td>
-</tr>
-<tr>
-<td>Notification of acceptance</td>
-<td>March 22nd, 2019</td>
-</tr>
-<tr>
-<td>Workshop Paper Submissions Deadline</td>
-<td>June 14th, 2019</td>
-<tr>
-<td>Camera-ready Workshop Papers Due</td>
-<td>August 12th, 2019</td>
-</tr>
-<tr>
-<td>Workshop Dates</td>
-<td>October 21 and October 25, 2019</td>
-</tr>
-</table>
-</div>
+<b>Tutorial Proposal Submission Instructions</b> <br/>
+The proposal should be no longer than three pages and should include at least the following:
+<ul style="text-align:left">
+<li>The name of the tutorial.</li>
+<li>The name, address, and short bio (up to 200 words) of each organizer (up to three).</li>
+<li>A motivation and rationale for the tutorial.</li>
+<li>Reference to the knowledge that the audience should expect to gather with the tutorial.</li>
+<li>An outline of the tutorial content, including the length of the tutorial (half day or full day) and a tentative schedule.</li>
+<li>Requirements for the tutorial room (please note that our capabilities in fulfilling unusual requests are limited).</li>
+<li>Requirements for the attendees (e.g., must bring their own laptop or other hardware, familiarity with certain technologies or topics, etc.).</li>
+<li>Any other limitations (e.g., number of participants).</li>
+<br/><br/>
 
-<h5>Workshop Chairs</h5>
-Jennifer Chen (Rutgers University): <a href="mailto:yingche@scarletmail.rutgers.edu">yingche@scarletmail.rutgers.edu</a><br/>
-Rajesh Balan (Singapore Management University, Singapore): <a href="mailto:rajesh@smu.edu.sg">rajesh@smu.edu</a><br/>
+<h5><b>Submissions</b></h5>
+Please send your proposal in PDF (only) via e-mail to both the workshop/tutorial co-chairs Özgü Alay and Lan Wang (contact details are provided below). Ensure that proposals contain "MobiCom 2020 Workshop/Tutorial Proposal" in the subject line. Submissions must be received by the deadline below.
+<br/><br/>
+
+<h5><b>Important Dates</b></h5>
+<?php require_once('dates-workshop.php'); ?>
+
+<h5><b>Workshop/Tutorial Chairs</b></h5>
+<a href="https://ozgualay.com/">Özgü Alay</a> (University of Oslo and Simula) : ozgua@ifi.uio.no<br/>
+<a href="https://www.memphis.edu/cs/people/faculty_pages/lan-wang.php">Lan Wang</a> (University of Memphis) : lanwang@memphis.edu
+<br/><br/>
+
+
+<h5><b>General Information</b></h5>
+ACM MobiCom provides basic support for the workshops and tutorials in the form of registration, publication, publicity, meeting space, audio/video equipment, lunch, and coffee breaks. To promote flexibility, the details including the format of the workshop/tutorial and the form of submissions and presentations are typically left to the organizers of each workshop/tutorial. Because workshops are meant to foster discussions on emerging topics of interest among the broader SIGMOBILE community, workshops are encouraged to be inclusive rather than exclusive in their solicitation of material. In addition, we encourage organizers to target more interactive forums such as presentations of short papers, posters and demos, panels, and round-table discussions, rather than traditional presentations of full-length papers.  Workshops/tutorials not attracting enough submissions and registrations may be cancelled at the decision of the organizing committee.
+<br/><br/>
+We look forward to your submissions. Please feel free to contact us with any questions about submitting a workshop/tutorial proposal.
 <br/>
-<h5>General Information</h5>
-ACM MobiCom provides basic support for the workshops in the form of registration, publication, publicity, meeting space, audio/video equipment, lunch, and coffee breaks. To promote flexibility, the details including the format of the workshop and the form of submissions and presentations are typically left to the organizers of each workshop. Because workshops are meant to foster discussions on emerging topics of interest among the broader SIGMOBILE community, workshops are encouraged to be inclusive rather than exclusive in their solicitation of material. In addition, we encourage organizers to target more interactive forums such as presentations of short papers, posters and demos, panels, and round-table discussions, rather than traditional presentations of full-length papers. We look forward to your submissions. Please feel free to contact us with any questions about submitting a workshop proposal.
+
+
+</main>
 
 </center>
-</main>
+
 <?php require_once('footer2.php'); ?>
 
 </body>
