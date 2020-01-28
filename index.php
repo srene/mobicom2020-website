a582c6efebad4a46f385c41323d4a7fcb801f929
srenevic <s.rene@ucl.ac.uk>
2020-01-28 09:58:06 +0100

    accepted papers

--- index.php
+++ index.php
@@ -104,9 +104,7 @@ On-Site (Begins Oct 21st)<br/><br/>
 </ul>
 </div>
 
-<div class="spacing"></div>
-<div class="spacing"></div>
-<div class="spacing"></div>
+</br></br>
 
 <!-- <?php require_once('sponsors.php'); ?> -->
 
