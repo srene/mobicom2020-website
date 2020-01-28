eda442a8d12e7e353f5d46688a153d4a447d44e9
srenevic <s.rene@ucl.ac.uk>
2020-01-28 09:56:08 +0100

    accepted papers

--- accepted.php
+++ accepted.php
@@ -15,8 +15,100 @@
 <ul>
 
 <li><b>From Relative Azimuth to Absolute Location: Pushing the Limit of PIR Sensor based Localization</b> <!--<a href="http://web.mit.edu/arjunvb/pubs/bursttracker-mobicom19-paper.pdf">[Preprint]</a>--><br/>
-<div class="pauthors">X. Liu, T. Yang, S. Tang, P. Guo, J. Niu</div></li>
+<div class="pauthors">Authors: X. Liu, T. Yang, S. Tang, P. Guo, J. Niu</div></li>
 <br/>
+
+<li><b>Millimeter-Wave Full Duplex Radios</b> <!--<a href="http://web.mit.edu/arjunvb/pubs/bursttracker-mobicom19-paper.pdf">[Preprint]</a>--><br/>
+<div class="pauthors">Authors: V. Singh, S. Mondal, A. Gadre, M. Srivastava, J. Paramesh, S. Kumar</div></li>
+<br/>
+
+<li><b>Redefining Passive in Backscattering with Commodity Devices</b> <!--<a href="http://web.mit.edu/arjunvb/pubs/bursttracker-mobicom19-paper.pdf">[Preprint]</a>--><br/>
+<div class="pauthors">Authors: M. Rostami, K. Sundaresan, E. Chai, S. Rangarajan, D. Ganesan</div></li>
+<br/>
+
+<li><b>AR-based Person Identification in Crowded Urban Spaces</b> <!--<a href="http://web.mit.edu/arjunvb/pubs/bursttracker-mobicom19-paper.pdf">[Preprint]</a>--><br/>
+<div class="pauthors">Authors: J. Yi, S. Choi, Y. Lee</div></li>
+<br/>
+
+<li><b>X-Array: Approximating Omnidirectional Millimeter-Wave Coverage Using an Array of Phased-Arrays</b> <!--<a href="http://web.mit.edu/arjunvb/pubs/bursttracker-mobicom19-paper.pdf">[Preprint]</a>--><br/>
+<div class="pauthors">Authors: S. Wang, J. Huang, X. Zhang, H. Kim, S. Dey</div></li>
+<br/>
+
+<li><b>Renovating Road Signs for I2V Communications</b> <!--<a href="http://web.mit.edu/arjunvb/pubs/bursttracker-mobicom19-paper.pdf">[Preprint]</a>--><br/>
+<div class="pauthors">Authors: P. Wang, L. Feng, G. Chen, C. Xu, Y. Wu, K. Xu, G. Shen, S. Shao</div></li>
+<br/>
+
+<li><b>Voice Localization Using Nearby Wall Reflections</b> <!--<a href="http://web.mit.edu/arjunvb/pubs/bursttracker-mobicom19-paper.pdf">[Preprint]</a>--><br/>
+<div class="pauthors">Authors: S. Shen, D. Chen, Y. Wei, Z. Yang, R. Choudhury</div></li>
+<br/>
+
+<li><b>Ghost Calls from Operational 4G Call Systems: IMS Vulnerability, Call DoS Attack, and Countermeasure</b> <!--<a href="http://web.mit.edu/arjunvb/pubs/bursttracker-mobicom19-paper.pdf">[Preprint]</a>--><br/>
+<div class="pauthors">Authors: Y. Lu, C. Li, Y. Li, S. Hsiao, T. Xie, G. Tu, W. Chen</div></li>
+<br/>
+
+<li><b>Hummingbird: Energy Efficient GPS Receiver for Small Satellites</b> <!--<a href="http://web.mit.edu/arjunvb/pubs/bursttracker-mobicom19-paper.pdf">[Preprint]</a>--><br/>
+<div class="pauthors">Authors: S. Narayana, R Prasad, V. Rao, L. Mottola, T Prabhakar</div></li>
+<br/>
+
+<li><b>ScatterMIMO: Enabling Virtual MIMO with Smart Surfaces</b> <!--<a href="http://web.mit.edu/arjunvb/pubs/bursttracker-mobicom19-paper.pdf">[Preprint]</a>--><br/>
+<div class="pauthors">Authors: M. Dunna, C. Zhang, D. Sievenpiper, D. Bharadia</div></li>
+<br/>
+
+<li><b>ViVo: Visibility-aware Mobile Volumetric Video Streaming</b> <!--<a href="http://web.mit.edu/arjunvb/pubs/bursttracker-mobicom19-paper.pdf">[Preprint]</a>--><br/>
+<div class="pauthors">Authors: B. Han, Y. Liu, F. Qian</div></li>
+<br/>
+
+<li><b>PDLens: Smartphone Knows Drug Effectiveness among Parkinson's via Daily-Life Activity Fusion</b> <!--<a href="http://web.mit.edu/arjunvb/pubs/bursttracker-mobicom19-paper.pdf">[Preprint]</a>--><br/>
+<div class="pauthors">Authors: H. Zhang, G. Guo, C. Song, C. Xu, K. Cheung, J. Alexis, H. Li, D. Li, K. Wang, W. Xu</div></li>
+<br/>
+
+<li><b>Integrating Device, Cloud, and Client Development for IoT Applications with OneLink</b> <!--<a href="http://web.mit.edu/arjunvb/pubs/bursttracker-mobicom19-paper.pdf">[Preprint]</a>--><br/>
+<div class="pauthors">Authors: G. Guan, B. Li, Y. Gao, Y. Zhang, J. Bu, W. Dong</div></li>
+<br/>
+
+<li><b>Challenge: UTOPIA: A City-Scale Programmable Testbed for Experimentation with Advanced Wireless</b> <!--<a href="http://web.mit.edu/arjunvb/pubs/bursttracker-mobicom19-paper.pdf">[Preprint]</a>--><br/>
+<div class="pauthors">Authors: D. Raychaudhuri, I. Seskar, G. Zussman, T. Korakis, D. Kilper, J. Kolodziejski, M. Sherman, T. Chen, Z. Kostic, X. Gu, H. Krishnaswamy, S. Maheshwari, P. Skrimponis, C. Gutterman</div></li>
+<br/>
+
+<li><b>M-Cube: A Millimeter-Wave Massive MIMO Software Radio</b> <!--<a href="http://web.mit.edu/arjunvb/pubs/bursttracker-mobicom19-paper.pdf">[Preprint]</a>--><br/>
+<div class="pauthors">Authors: R. Zhao, T. Woodford, T. Wei, Q. Kun, X. Zhang</div></li>
+<br/>
+
+<li><b>Experience: Advanced Network Operations in (Un)-Connected Remote Communities</b> <!--<a href="http://web.mit.edu/arjunvb/pubs/bursttracker-mobicom19-paper.pdf">[Preprint]</a>--><br/>
+<div class="pauthors">Authors: D. Perino, X. Yang, J. Serra, A. Lutu, I. Leontiadis</div></li>
+<br/>
+
+<li><b>Deep Learning based Wireless Localization for Indoor Navigation</b> <!--<a href="http://web.mit.edu/arjunvb/pubs/bursttracker-mobicom19-paper.pdf">[Preprint]</a>--><br/>
+<div class="pauthors">Authors: R. Ayyalasomayajula, A. Arun, C. Wu, S. Sharma, A. Sethi, D. Vasisht, D. Bharadia</div></li>
+<br/>
+
+<li><b>Single Shot Single Antenna Path Discovery in THz Networks</b> <!--<a href="http://web.mit.edu/arjunvb/pubs/bursttracker-mobicom19-paper.pdf">[Preprint]</a>--><br/>
+<div class="pauthors">Authors: Y. Ghasempour, C. Yeh, R. Shrestha, D. Mittleman, E. Knightly</div></li>
+<br/>
+
+<li><b>MET: A Novel Magneto-Inductive Sensing Based Electric Toothbrushing Monitoring System</b> <!--<a href="http://web.mit.edu/arjunvb/pubs/bursttracker-mobicom19-paper.pdf">[Preprint]</a>--><br/>
+<div class="pauthors">Authors: H. Huang, S. Lin</div></li>
+<br/>
+
+<li><b>Experience: Aging or Glitching? Why Does Android Stop Responding and What Can We Do About It?</b> <!--<a href="http://web.mit.edu/arjunvb/pubs/bursttracker-mobicom19-paper.pdf">[Preprint]</a>--><br/>
+<div class="pauthors">Authors: M. Li, H. Lin, Z. Li, F. Qian, T. Xu, C. Liu, Y. Liu</div></li>
+<br/>
+
+<li><b>WiChronos : Energy-Efficient Modulation for Long-Range, Large-Scale Wireless Networks</b> <!--<a href="http://web.mit.edu/arjunvb/pubs/bursttracker-mobicom19-paper.pdf">[Preprint]</a>--><br/>
+<div class="pauthors">Authors: Y. Sangar, B. Krishnaswamy</div></li>
+<br/>
+
+<li><b>Towards Flexible Wireless Charging for Medical Implants Using Distributed Antenna System</b> <!--<a href="http://web.mit.edu/arjunvb/pubs/bursttracker-mobicom19-paper.pdf">[Preprint]</a>--><br/>
+<div class="pauthors">Authors: X. Fan, L. Shangguan, R. Howard, Y. Zhang, Y. Peng, J. Xiong, Y. Ma, X.-Y. Li</div></li>
+<br/>
+
+<li><b>3D Human Pose Construction Using WiFi</b> <!--<a href="http://web.mit.edu/arjunvb/pubs/bursttracker-mobicom19-paper.pdf">[Preprint]</a>--><br/>
+<div class="pauthors">Authors: W. Jiang, H. Xue, C. Miao, S. Wang, S. Lin, C. Tian, S. Murali, H. Hu, Z. Sun, L. Su</div></li>
+<br/>
+
+<li><b>TouchPass: Towards Behavior-irrelevant on-touch User Authentication on Smartphones Leveraging Vibrations</b> <!--<a href="http://web.mit.edu/arjunvb/pubs/bursttracker-mobicom19-paper.pdf">[Preprint]</a>--><br/>
+<div class="pauthors">Authors: X. Xu, J. Yu, Y. Chen, Q. Hua, Y. Zhu, Y. Chen, M. Li</div></li>
+<br/> 
 
 </ul>
 </div>
