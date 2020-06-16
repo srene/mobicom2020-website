87ca924d38a760064536fa167a23ae117bcbfc71
srenevic <s.rene@ucl.ac.uk>
2020-06-16 12:38:32 +0200

    call for posters

---
+++ poster.php
@@ -0,0 +1,162 @@
+<?php require_once('head.php'); ?>
+
+<body>
+
+<center>
+<?php require_once('nav.php'); ?>
+<?php require_once('banner-small.php'); ?>
+
+<div class="container bigc">
+<div class="spacing"></div>
+
+<main>
+
+<h2 class="text-center">Call for Posters</h2>
+<!--<center>TBC</center>
+</br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br>-->
+Following the long tradition of poster presentations, ACM MobiCom 2020 solicits submissions for Posters Program that feature early research work and preliminary results. The MobiCom 2020 Poster Session will provide an excellent opportunity for initial feedback on your early research results and give your ongoing projects early visibility. The presenters are strongly encouraged to include their poster submission in the <a href="https://sigmobile.org/mobicom/2020/src.php">ACM MobiCom Student Research Competition</a>.<br/><br/>
+Posters are solicited in all areas of mobile computing and wireless networking. Particular areas of interest include, but are not limited to:
+<ul style="text-align:left">
+<li>Fundamental understanding of mobile computing and wireless networking</li>
+<li>Applications of machine learning to mobile/wireless research</li>
+<li>Backscatter communication and wireless power</li>
+<li>Mobile edge computing and cloud computing for mobile services Embedded and energy-harvesting systems</li>
+<li>Experimental platforms and infrastructures for wireless/mobile research</li>
+<li>Implanted and wearable computing</li>
+<li>Intersection between mobile/wireless research, cloud computing, and software-defined networking</li>
+<li>Long-range/Low-power wide-area wireless networking</li>
+<li>Low-latency networking</li>
+<li>Machine-to-machine communications</li>
+<li>Millimeter wave and terahertz communications</li>
+<li>Mobile data science & analysis</li>
+<li>Mobile health</li>
+<li>Mobile web, video, virtual reality, and other applications</li>
+<li>Next-generation 5G mobile networks</li>
+<li>Novel applications of wireless signals</li>
+<li>Robotic and drone-based networking</li>
+<li>Security and privacy issues/solutions for mobile/wireless systems</li>
+<li>Sensing with radio, light, sound, and acoustics</li>
+<li>Ubiquitous computing and mobile human computer interaction</li>
+<li>Visible light communications</li>
+<li>Wireless localization and tracking</li>
+<li>Experimental large-scale testbeds for mobile networks</li>
+<li>Performance evaluation of mobile networks, protocols, and systems</li>
+</ul>
+
+<h5><b>Poster Submission instructions</b></h5>
+The posters should be submitted via hotCRP: <a href="https://mobicom20posters.hotcrp.com/">https://mobicom20posters.hotcrp.com/</a></br></br>
+
+The submission should be formatted as a paper or extended abstract, describing the research to be presented in the poster and should be submitted as a PDF file of at most 3 pages, including all figures and references.
+Paper text blocks must follow ACM guidelines: double-column US letter size of 8.5 x 11 inches with fonts no smaller than 10 point size. The extended abstract must include the authors' names, affiliations, and email addresses. Any submission should include the following:
+<ul style="text-align:left">
+<li>Whether you would like the submission to be considered for the Student Research Competition</li>
+<li>If you would like your submission to be considered for the SRC, there is the additional need to indicate whether any of the student authors is an undergraduate student. Note that if any of the authors is a graduate student, then the submission will be considered under the graduate category</li>
+</ul>
+</br></br>
+All poster submissions will be reviewed by the Posters committee. If your submission is selected for the Poster Session, you should follow the recommendations to prepare and produce your “virtual poster”.  Once your poster is accepted, we will include the abstract of your poster on the conference website and publish it in the main proceedings and the ACM digital library.
+</br></br>
+<h5><b>Virtual Poster Session Instructions</b></h5>
+We will organize a <b><em>virtual poster session</em></b> as part of the conference program.
+For each of the selected submissions for the poster session, we ask you to prepare the following:
+ul style="text-align:left">
+<li>A pre-recorded 1-minute presentation to be played during the poster session (part of the conference program); we will organize a live chat channel (e.g., using Slack), where the authors can receive questions and engage in discussions with the attendees.</li>
+<li>A pre-recorded video of maximum 5 minutes explaining the poster at length, to be available offline on the conference webpage.</li>
+</ul>
+</br></br>
+
+<h5><b>Important Dates:</b></h5>
+<div class="table-responsive">
+<table class="table table-striped table-sm">
+<tr>
+<th>Deadlines</th>
+<th>Dates</th>
+</tr>
+<tr>
+<td>Poster Submission Deadline</td>
+<td>July 6th</td>
+</tr>
+<tr>
+<td>Notification</td>
+<td>July 23th</td>
+<tr>
+<td>Camera-ready</td>
+<td>July 31st</td>
+</tr>
+</table>
+</div>
+</br></br>
+<h5><b>Contact</b></h5>
+For further questions, please contact the Poster co-chairs:
+Giovanni Pau (giovanni.pau _at_ unibo.it)
+Andra Lutu (andra.lutu _at_ telefonica.com)
+
+
+
+
+<!--
+The MobiCom demo and poster sessions solicit the submission of research, platform, and product demonstrations and posters from both academia and industry.<br/><br/>
+
+Demonstrations should showcase innovative research or product prototypes, platforms, and applications in the areas of mobile computing and wireless and mobile networking. We strongly encourage live​ demos based on novel applications in the domain of mobile computing and wireless systems. We also welcome live​ demos reproducing evaluation results of papers appearing in ACM Mobicom 2019 or past editions as well as other relevant venues.<br/><br/>
+
+The MobiCom 2019 Poster Session will provide an excellent opportunity for initial feedback on your early research results and give your ongoing projects early visibility. ACM MobiCom 2019 will host an ACM <a href="src.php">Student Research Competition (SRC)</a> alongside the Posters Program. SRC offers graduate and undergraduate students an opportunity to present their original research, and get feedback. The presenters are strongly encouraged to include their poster submission for the SRC.<br/><br/>
+
+The MobiCom 2019 Demo and Poster committee will review all submissions to select the accepted demos and posters. All accepted submissions will be considered for Best Demo and Best Poster Awards. A committee will decide the winner on-site after evaluating all presentations.<br/><br/>
+
+Please refer to the main conference introduction and <a href="cfp.php">CFP</a> for the general areas that are relevant to the typical MobiCom audience.<br/><br/>
+
+<h5><b>Why should you submit a Demo or Poster?</b></h5>
+Presenting a demo or poster is a great opportunity, especially for students and industry, to obtain valuable feedback on ongoing research from the knowledgeable crowd at the conference. Accepted submissions will be posted on the conference website and published as a three-page abstract for the archived conference proceedings and the ACM digital library.<br/><br/>
+
+<h5><b>Where and What to Submit?</b></h5>
+Please submit a proposal of your demo or poster at <a href="https://mobicom19demos-posters.hotcrp.com/">https://mobicom19demos-posters.hotcrp.com/</a>. It should be a summary or extended abstract describing the research to be presented, maximum 3 pages in US letter size (8.5 x 11 inches) with font no smaller than 10 point size, in PDF file format. Please ensure that the proposal includes:<br/><br/>
+
+The names, affiliations, and email addresses of the authors, and whether the submission should preferably be considered for the demo session, or the poster session. If presenting a demo, mention the equipment to be used for the demo (check default setup below), the space and setup time required, and any additional facilities needed including estimated power connections needed for the demo, special environments and tools, Internet access, physical space requirements, etc.<br/><br/>
+
+<h5><b>Can I also include a poster when presenting the demo?</b></h5>
+Yes. All accepted demo submissions would also be provided with a single easel for displaying the accompanied poster.<br/><br/>
+
+
+<h5><b>Default Setup:</b></h5>
+For all accepted submissions in demo or poster sessions, a single easel for displaying a poster will be provided. Suggested poster size is 3 x 3 ft, or 3 x 4 ft in portrait orientation. By no means should the poster exceed 4x6 ft in portrait orientation.<br/><br/>
+
+For submissions accepted for demos, additionally a single 6 x 2.5 ft table would be provided. Demo participants should bring extension power cable/multi-point along with suitable power adapters and voltage transformers.<br/><br/>
+
+<h5><b>SRC instructions:</b></h5>
+Indicate whether you would like the poster submission to be considered for the Student Research Competition (SRC). Indicate your correct identifier by adding [SRC/Undergraduate] or <b>[SRC/Graduate]</b> in the submission title for consideration in respective SRC category. Note that if any of the authors is a graduate student, then the submission will be considered under the graduate category.<br/><br/>
+
+<h5><b>Important Dates:</b></h5>
+<div class="table-responsive">
+<table class="table table-striped table-sm">
+<tr>
+<th>Deadlines</th>
+<th>Dates</th>
+</tr>
+<tr>
+<td>Submission</td>
+<td>June 15, 2019 11:59 PM</td>
+</tr>
+<tr>
+<td>Notification of acceptance</td>
+<td>June 24, 2019 11:59 PM</td>
+<tr>
+<td>Camera-ready</td>
+<td>July 1, 2019</td>
+</tr>
+</table>
+</div>
+
+<b>For further questions, please contact the chairs:</b>
+<a href="mailto:mobicom19demos-posters@googlegroups.com">mobicom19demos-posters@googlegroups.com</a>
+<br/><br/>
+<h5><b>Important Notes:</b></h5>
+AUTHORS TAKE NOTE: The official publication date is the date the proceedings are made available in the ACM Digital Library. This date may be up to two weeks prior to the first day of your conference. The official publication date affects the deadline for any patent filings related to published work. (For those rare conferences whose proceedings are published in the ACM Digital Library after the conference is over, the official publication date remains the first day of the conference.)
+
+<br/>-->
+
+</main>
+
+</center>
+<?php require_once('footer2.php'); ?>
+
+</body>
+</html>
