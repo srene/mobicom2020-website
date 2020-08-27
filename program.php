6e70028fbf211cea05696e61ac8ceecfc3973466
srenevic <s.rene@ucl.ac.uk>
2020-08-27 09:11:45 +0200

    program

--- program.php
+++ program.php
@@ -22,10 +22,7 @@
 <p>Bozidar and Qian</p>
 </div>
 
-<!--<?php require_once('program-table.php'); ?>-->
-
-<iframe src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=Europe%2FLondon&amp;src=c3JlbmV2aWNAZ21haWwuY29t&amp;src=MGNjN2U2N240b24xanFpNTJoOGkyc283cnNAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;src=ZW4uc3BhaW4jaG9saWRheUBncm91cC52LmNhbGVuZGFyLmdvb2dsZS5jb20&amp;src=b3V0bGllcnZlbnR1cmVzLmlvX3Y4aWMxdDcxYW9zanZuNzRuNjIzaTZ1MWg4QGdyb3VwLmNhbGVuZGFyLmdvb2dsZS5jb20&amp;color=%234285F4&amp;color=%23D81B60&amp;color=%237986CB&amp;color=%237CB342" style="border:solid 1px #777" width="800" height="600" frameborder="0" scrolling="no"></iframe>
-
+<iframe src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=2&amp;bgcolor=%23ffffff&amp;ctz=America%2FLos_Angeles&amp;src=MGNjN2U2N240b24xanFpNTJoOGkyc283cnNAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;color=%23D81B60&amp;showNav=0&amp;showPrint=0&amp;showDate=0&amp;showCalendars=0" style="border-width:0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
 
 
 </center>
