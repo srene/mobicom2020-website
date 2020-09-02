62e4ebad2824c2a675137c42ae5722524d7d744e
srenevic <s.rene@ucl.ac.uk>
2020-09-02 08:22:51 +0200

    mentorship

---
+++ mentorship.php
@@ -0,0 +1,76 @@
+<?php require_once('head.php'); ?>
+
+<body>
+
+<center>
+<?php require_once('nav.php'); ?>
+<?php require_once('banner-small.php'); ?>
+
+<div class="container bigc">
+<div class="spacing"></div>
+
+<main>
+<h2 class="text-center">ACM MobiCom 2020 Mentorship Program </h2>
+</br>
+<h3 class="text-center">Call for Applications</h3>
+</br>
+<style type="text/css">
+ span.bold-red {
+    color: red;
+    font-weight: bold;
+    font-size: large;
+}
+</style>
+
+SIGMOBILE is launching its second Mentorship Program at MobiCom 2020. The goal of this program  is to enable SIGMOBILE student members to get a chance to interact with senior researchers, either one-on-one or as a small team of 2 students. All mentoring sessions will be conducted online.
+<br/><br/>
+
+To be eligible for participation, you must be:
+<ol style="text-align:left" type="a">
+<li>currently enrolled as a graduate student (M.S. or PhD)</li>
+<li>a registered attendee at ACM MobiCom’20</li>
+<li>available on all days of the main conference: Sep 21 - Oct 24</li>
+</ol>
+
+Applications are now open. If you are interested, please fill the <a href="https://docs.google.com/forms/d/e/1FAIpQLSc2RNXyt7ZilLorecdEJCmnjCgFp2yP-9hsuB3zahHslvBcgw/viewform">Mentorship Program Application Form</a> by September 7, 2020. The application requires you to upload your most recent CV and a Mentorship Statement. The Mentorship Statement must be no longer than one US letter-sized paper (8.5in X 11 in), with font size no smaller than 10 points. Your statement must consist of the following:
+<ol style="text-align:left" type="a">
+<li><i>Research interests</i></li>
+<li><i>Expectations from the program:</i> If you have specific mentoring needs, please specify here. For example, please indicate if you are currently applying to industry and/or academia and are looking for a mentor to guide you in that aspect.</li>
+<li><i>Who would you like to meet:</i> If you would like a specific researcher as your mentor, please indicate so. We cannot guarantee that you will be paired with a mentor of your choice.</li>
+<li><i>vailability:</i>Please indicate if you are available on Workshop Days in addition to the Main Conference Days.</li>
+</ol>
+
+<h5><b>Important Dates:</b></h5>
+<div class="table-responsive">
+<table class="table table-striped table-sm">
+<tr>
+<th>Deadlines</th>
+<th>Dates</th>
+</tr>
+<tr>
+<td>Applications Due</td>
+<td>September 10th 2020</td>
+</tr>
+<tr>
+<td>Acceptance Notifications</td>
+<td>September 15th 2020</td>
+<tr>
+<td>Mentoring Schedule</td>
+<td>September 15th 2020</td>
+</tr>
+</table>
+</div>
+</br>
+
+For further questions, please contact Mariya Zheleva or Shubham Jain at: </br>
+mobicom2020mentorship@gmail.com</br></br>
+
+We hope you will enjoy this wonderful opportunity. We look forward to seeing you at the virtual MobiCom!</br></br>
+</main>
+
+</center>
+
+<?php require_once('footer2.php'); ?>
+
+</body>
+</html>
--- nav.php
+++ nav.php
@@ -71,6 +71,9 @@
       <li class="nav-item">
         <a class="nav-link" href="sponsorships.php">Sponsorships</a>
       </li>
+      <li class="nav-item">
+        <a class="nav-link" href="mentorship.php">Menthorship</a>
+      </li>
     </li>
     <li class="nav-item">
       <a class="nav-link" href="video.php">Presentation Instructions</a>
