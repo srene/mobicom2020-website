1c2411b475e22b76286f3dbb8fd7e109416a2d74
srenevic <s.rene@ucl.ac.uk>
2020-09-07 14:00:26 +0200

    program

--- program.php
+++ program.php
@@ -29,21 +29,61 @@ div.centre {
 <p>Along with an amazing technical program, we will have an assortment of exciting keynotes and panels. We hope you will enjoy!</p>
 </br>
 <p>Bozidar and Qian</p>
-</div>
 
-<span class="bold-red">The program is provisional and is likely to change a bit until the final version.</span>
+<!--<span class="bold-red">The program is provisional and is likely to change a bit until the final version.</span>-->
 <div class="centre" id="calendar-container"></div>
 
 <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jstimezonedetect/1.0.4/jstz.min.js"></script>
 <script type="text/javascript">
  var timezone = encodeURIComponent(jstz.determine().name());
- var pref = '<iframe src="https://calendar.google.com/calendar/embed?height=800&amp;wkst=2&amp;bgcolor=%23ffffff&amp;src=MGNjN2U2N240b24xanFpNTJoOGkyc283cnNAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;color=%23D81B60&amp;showDate=1&amp;showNav=0&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;mode=AGENDA&amp;ctz=';
- var suff = '" style=" border-width:0 " width="1000" height="800" frameborder="0" scrolling="no"></iframe>';
+ var pref = '<iframe src="https://calendar.google.com/calendar/embed?height=850&amp;wkst=2&amp;bgcolor=%23ffffff&amp;src=MGNjN2U2N240b24xanFpNTJoOGkyc283cnNAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;color=%23D81B60&amp;showDate=1&amp;showNav=0&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;mode=AGENDA&amp;ctz=';
+ var suff = '" style=" border-width:0 " width="1000" height="850" frameborder="0" scrolling="no"></iframe>';
  var iframe_html = pref + timezone + suff;
  document.getElementById('calendar-container').innerHTML = iframe_html;
 </script>
 
+</br>
+<h5><b>Keynote speakers</b></h5>
+<b>Keynote 1: Electronics on the brain</b><br/>
+<b>George Malliaras. Department of Engineering, University of Cambridge.</b>
+<br/>
+<p>Abstract: One of the most important scientific and technological frontiers of our time is the interfacing of electronics with the human brain. This endeavour promises to help understand how the brain works and deliver new tools for diagnosis and treatment of pathologies including epilepsy and Parkinson’s disease. Current solutions, however, are limited by the materials that are brought in contact with the tissue and transduce signals across the biotic/abiotic interface. Recent advances in electronics have made available materials with a unique combination of attractive properties, including mechanical flexibility, mixed ionic/electronic conduction, enhanced biocompatibility, and capability for drug delivery. I will present examples of novel devices for recording and stimulation of neurons and show that organic electronic materials offer tremendous opportunities to study the brain and treat its pathologies.
+</p>
+<center><img src="images/keynote/malliaras.png" height="200"></center>
+<br/>
+<p>George Malliaras is the Prince Philip Professor of Technology at the University of Cambridge (UK). He received a PhD from the University of Groningen (the Netherlands) and did a postdoc at the IBM Almaden Research Center (USA). Before joining Cambridge, he was a faculty member at School of Mines (France) and at Cornell University (USA), and served as the Director of the Cornell NanoScale Facility. His research has been recognized with awards from the New York Academy of Sciences, the US National Science Foundation, and DuPont, and an Honorary Doctorate from the University of Linköping in Sweden. He is a Fellow of the Materials Research Society and of the Royal Society of Chemistry and serves as Deputy Editor of Science Advances.</p>
+</br>
 
+<b>Keynote 2: TBD</b><br/>
+<b>Sachin Katti. Stanford University.</b><br/>
+<p>Abstract: TBD.
+<center><img src="images/keynote/katti.png" height="200"></center>
+<br/>
+<p>Sachin Katti is an Associate Professor of Electrical Engineering and Computer Science at Stanford University and an Advisor in charge of Telco Strategy at VMware. He is also the Co-Chair of the Technical Steering Committee for O-RAN Alliance and the Director of the xRAN Foundation. He is also Co-Founder and ex-CEO of Uhana (now part of VMware) which built a network AI platform to monitor and optimize mobile networks and applications. He has previously co-founded Kumu Networks, which is commercializing breakthrough research from his lab on full duplex radios. He received his PhD in EECS from MIT in 2009. His research focuses on designing and building next generation high capacity wireless networks by combining techniques from information and coding theory, RF systems, and networking.  His research has won numerous awards, including the 2008 ACM Doctoral Dissertation Award - Honorable Mention, the George Sprowls Award for Best Doctoral Dissertation in EECS at MIT, the IEEE William Bennett Prize, the Best Student Paper Award at ACM SIGCOMM 2012, USENIX ATC 2013, the Sloan Fellowship, the NSF Career Award as well as Okawa, Hooover, Packard and Terman Faculty Fellowships.</p></br>
+
+<b>Keynote 3: What’s next in the cellular infrastructure</b><br/>
+<b>Durga Malladi. Qualcomm Technologies.</b>
+</br>
+<p>Abstract:</p>
+<ul>
+  <li>5G Network Topology</li>
+  <li>Challenges with diversity of networks, venues, city-wide, enterprise, industrial deployments spanning public & private domains</li>
+  <li>New paradigms with open interfaces, virtualization and COTS</li>
+  <li>High performance radios, inline accelerators</li>
+</ul>
+
+<center><img src="images/keynote/malladi.png" height="200"></center>
+<br/>
+<p>Durga Malladi is Senior Vice President and General Manager, 4G/5G in Qualcomm Technologies, Inc.
+Durga joined Qualcomm in 1998 as a Senior Engineer. Over the past 22 years, he has worked on 3G, 4G and 5G technologies in various R&D and Business roles.
+He was the Project Engineer of 4G LTE and LTE-Advanced in Qualcomm Research from 2008-15, working on features such as Carrier Aggregation, Small cells, HetNets, Relays, Licensed Assisted Access (LAA), Internet of Things (eMTC, NB-IoT) and Cellular V2X.
+From 2015-18, he led 5G Research in Qualcomm across Sub6 and Millimeter Wave bands, focusing on enhanced Mobile Broadband (eMBB), Ultra Reliable Low Latency Communications (URLLC), Industrial IoT, 5G based Vehicular Communications (C-V2X), Integrated Access and Backhaul systems, Unlicensed / Shared Spectrum Access and Massive IoT (mIoT). The responsibilities spanned system design, standardization, prototype testbeds, pre-commercial vendor inter-operability tests and trials.
+Since 2018, as Senior VP & General Manager of 4G/5G, he is responsible for Cellular Modem, Position Location and Infrastructure Modem-RF businesses, Baseband and Transceiver modem technology roadmap & features, Software products, and Commercial engagements with 5G ecosystem partners (operators, service providers, infrastructure). He drives the overall 5G roadmap and solutions across business units, spanning Mobile, Industrial and Automotive segments.
+He is the recipient of Qualcomm’s IP Excellence Award, Qualcomm Distinguished Contributor Award for Project Leadership and Upendra Patel Achievement Awards for Outstanding Contributions to HSPA and LTE.
+Durga holds a B.Tech from Indian Institute of Technology, Madras, and an M.S and Ph.D. from UCLA. His Ph.D. dissertation is on Sequential Detection and Adaptive Estimation techniques, and his research interests include Signal Processing, Communication Theory and Artificial Intelligence. He is a member of IEEE and holds 505 U.S. patents.
+</p></br>
+
+</div>
 
 </center>
 </main>
