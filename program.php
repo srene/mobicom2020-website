626ad4d684298c572de315030994ee2e4d46e445
srenevic <s.rene@ucl.ac.uk>
2020-09-09 22:05:01 +0200

    keynote

--- program.php
+++ program.php
@@ -32,6 +32,17 @@
 
 <br/>
 
+<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jstimezonedetect/1.0.4/jstz.min.js"></script>
+<script type="text/javascript">
+ var timezone = encodeURIComponent(jstz.determine().name());
+ var pref = '<iframe src="https://calendar.google.com/calendar/embed?height=850&amp;wkst=2&amp;bgcolor=%23ffffff&amp;src=MGNjN2U2N240b24xanFpNTJoOGkyc283cnNAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;color=%23D81B60&amp;showDate=1&amp;showNav=0&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;mode=AGENDA&amp;ctz=';
+ var suff = '" style=" border-width:0 " width="1000" height="850" frameborder="0" scrolling="no"></iframe>';
+ var iframe_html = pref + timezone + suff;
+ document.getElementById('calendar-container').innerHTML = iframe_html;
+</script>
+
+</br>
+
 
 <div class="container bigc">
 <div style="margin-bottom:-50px;">
