a412dc826ee95c39f004fc9e574c36d1116ac4fa
srenevic <s.rene@ucl.ac.uk>
2020-09-10 23:11:09 +0200

    panel

---
+++ panel.php
@@ -0,0 +1,55 @@
+<?php require_once('head.php'); ?>
+
+<body>
+
+<center>
+<?php require_once('nav.php'); ?>
+<?php require_once('banner-small.php'); ?>
+
+<div class="container bigc">
+<div class="spacing"></div>
+
+<main>
+<h2 class="text-center">Panels</h2>
+<br/>
+<h5 align="center"><div id="panel1"></div><b>Panel 1: Future of Wi-Fi and 5G sensing and localization</i></h5>
+<h5 align="center"><b>Moderator: Yasamin Mostofi</b></h5>
+<h5 align="center"><i>Tuesday, 22nd September, 10:05-11:00 PDT</i></h5>
+<br/>
+
+<p>
+Panelists:
+<ol>
+<li>Li Qiu, Professor at Computer Science Dept. in UT Austin.</li>
+<li>Ranveer Chandra, Chief Scientist at Microsoft Azure Global.</li>
+<li>Shyam Gollakota, Associate Professor of Computer Science & Engineering at the Universityof Washington.</li>
+<li>Beibei Wang, Vice President of Research and Director ofIntellectual Properties at Origin Wireless Inc.</li>
+<li>Yasamin Mostofi, professor in the Department of Electrical and Computer Engineering atthe University of California Santa Barbara.</li>
+</ol>
+</p>
+</br>
+<hr/>
+
+<h5 align="center"><div id="panel2"></div><b>Panel 2: 5G and Edge Applications</i></h5>
+<h5 align="center"><b>Moderator: Lili Qiu</b></h5>
+<h5 align="center"><i>Thursday, 24th September, 09:20-10:20 PDT</i></h5>
+<br/>
+<p>
+Panelists:
+<ol>
+<li>Bo Han, Prof at George Mason Uni, formerly AT&T.</li>
+<li>Derek Long (Head of Telecoms and Mobile) and Fraser Edwards (Product Development), Cambridge Wireless.</li>
+<li>Ganesh Ananthanarayanan, Principal researcher at Microsoft.</li>
+<li>John Smee, VP of engineering at Qualcomm.</li>
+</ol>
+</p>
+</br>
+</br>
+</main>
+
+</center>
+
+<?php require_once('footer2.php'); ?>
+
+</body>
+</html>
